98663d5a75fb83474cad6a9275e9620e453c12a0
seansom <seanartsom@gmail.com>
2022-02-13 10:04:12 +0800

    added function to get dataframe of trading days in a specified date range, modified functions accordingly

--- get_technical_data.py
+++ get_technical_data.py
@@ -23,6 +23,19 @@ def get_dates_five_years(testing=False):
     return (date_five_years_ago.strftime('%Y-%m-%d'), date_today.strftime('%Y-%m-%d'))
 
 
+def get_trading_dates(stock_ticker, date_range, token):
+    exchange = 'PSE'
+    url = f"https://eodhistoricaldata.com/api/eod/{stock_ticker}.{exchange}?api_token={token}&order=a&fmt=json&from={date_range[0]}&to={date_range[1]}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    # convert to pd.dataframe
+    data = pd.json_normalize(data)
+
+    return data['date']
+
+
 def get_technical_indicators(data):
     """Computes for log stock returns and technical indicators unavailable to EOD (e.g., A/D, CMF, WR).
     Args:
@@ -64,15 +77,20 @@ def get_technical_indicator_from_EOD(indicator, period, token, stock_ticker, exc
         exchange (str): The stock exchange where the stock is being traded (e.g., PSE)
         date_range (tuple): A tuple of strings indicating the start and end dates for the requested data.
 
+    Raises:
+        Exception: Raises exception whenever gathered indicator data is insufficient to cover the specified
+        date range. This may be fixed by increasing the timedelta used in computing the adjusted_first_day variable
+        within the function.
+
     Returns:
         pd.Dataframe: Dataframe containing dates and specified technical indicator data
     """
 
     # compute an adjusted from or start date for the API
-    original_from_date = datetime.datetime.strptime(date_range[0],'%Y-%m-%d')
-    adjusted_from_date = original_from_date - datetime.timedelta(days=50)
+    first_trading_day_datetime = datetime.datetime.strptime(date_range[0],'%Y-%m-%d')
+    adjusted_first_day = ((first_trading_day_datetime) - datetime.timedelta(days=50)).strftime('%Y-%m-%d')
     
-    url = f"https://eodhistoricaldata.com/api/technical/{stock_ticker}.{exchange}?order=a&fmt=json&from={adjusted_from_date.strftime('%Y-%m-%d')}&to={date_range[1]}&function={indicator}&period={period}&api_token={token}"
+    url = f"https://eodhistoricaldata.com/api/technical/{stock_ticker}.{exchange}?order=a&fmt=json&from={adjusted_first_day}&to={date_range[1]}&function={indicator}&period={period}&api_token={token}"
 
     response = requests.get(url)
     data = response.json()
@@ -87,7 +105,7 @@ def get_technical_indicator_from_EOD(indicator, period, token, stock_ticker, exc
         curr_date = datetime.datetime.strptime(row['date'],'%Y-%m-%d')
 
         # remove unneeded earlier row entries
-        if curr_date < original_from_date:
+        if curr_date < first_trading_day_datetime:
             data.drop(index, inplace=True)
 
         else:
@@ -95,6 +113,12 @@ def get_technical_indicator_from_EOD(indicator, period, token, stock_ticker, exc
 
     # reset indices after dropping rows
     data = data.reset_index(drop=True)
+
+
+    # raise an exception if the data from EOD API is insufficient to cover the date range specified
+    # this error may be fixed by increasing timedelta used in computing adjusted_first_day
+    if data['date'][0] != date_range[0]:
+        raise Exception(f'Error getting {indicator} indicator for {stock_ticker}.')
 
     return data
 
@@ -114,22 +138,26 @@ def get_technical_data(stock_ticker, date_range):
         pd.Dataframe: Dataframe representing the dates, log stock returns, and technical indicators.
     """
 
-    # compute an adjusted from or start date for the API. Extra days worth of data is included
-    # to compute for log stock returns and technical indicators with extended periods
-    original_from_date = datetime.datetime.strptime(date_range[0],'%Y-%m-%d')
-    adjusted_from_date = original_from_date - datetime.timedelta(days=21)
-
     # get API key/token from txt file
     with open('keys/EOD_API_key.txt') as file:
         token = file.readline()
 
+    # get first and last trading days in the specified date range
+    trading_days = get_trading_dates(stock_ticker, date_range, token)
+    first_trading_day = trading_days[0]
+    last_trading_day = trading_days.iat[-1]
+
+    # adjust and add days to first trading day to be able to compute indicators with periods
+    first_trading_day_datetime = datetime.datetime.strptime(first_trading_day,'%Y-%m-%d')
+    adjusted_first_day = (first_trading_day_datetime - datetime.timedelta(days=30)).strftime('%Y-%m-%d')
+
     exchange = 'PSE'
-    url = f"https://eodhistoricaldata.com/api/eod/{stock_ticker}.{exchange}?api_token={token}&order=a&fmt=json&from={adjusted_from_date.strftime('%Y-%m-%d')}&to={date_range[1]}"
+    url = f"https://eodhistoricaldata.com/api/eod/{stock_ticker}.{exchange}?api_token={token}&order=a&fmt=json&from={adjusted_first_day}&to={last_trading_day}"
 
     response = requests.get(url)
     data = response.json()
 
-     # convert to pd.dataframe
+    # convert to pd.dataframe
     data = pd.json_normalize(data)
 
     # compute returns and technical indicators not available on EOD
@@ -140,7 +168,7 @@ def get_technical_data(stock_ticker, date_range):
     for index, row in data.iterrows():
         curr_date = datetime.datetime.strptime(row['date'],'%Y-%m-%d')
 
-        if curr_date < original_from_date:
+        if curr_date < first_trading_day_datetime:
             data.drop(index, inplace=True)
 
         else:
@@ -150,16 +178,16 @@ def get_technical_data(stock_ticker, date_range):
     data = data.reset_index(drop=True)
 
     # get available technical indicators from API
-    atr = get_technical_indicator_from_EOD('atr', 14, token, stock_ticker, exchange, date_range)
+    atr = get_technical_indicator_from_EOD('atr', 14, token, stock_ticker, exchange, (first_trading_day, last_trading_day))
     data = data.merge(atr, on='date')
 
-    rsi = get_technical_indicator_from_EOD('rsi', 14, token, stock_ticker, exchange, date_range)
+    rsi = get_technical_indicator_from_EOD('rsi', 14, token, stock_ticker, exchange, (first_trading_day, last_trading_day))
     data = data.merge(rsi, on='date')
 
-    cci = get_technical_indicator_from_EOD('cci', 20, token, stock_ticker, exchange, date_range)
+    cci = get_technical_indicator_from_EOD('cci', 20, token, stock_ticker, exchange, (first_trading_day, last_trading_day))
     data = data.merge(cci, on='date')
 
-    adx = get_technical_indicator_from_EOD('adx', 14, token, stock_ticker, exchange, date_range)
+    adx = get_technical_indicator_from_EOD('adx', 14, token, stock_ticker, exchange, (first_trading_day, last_trading_day))
     data = data.merge(adx, on='date')
 
     # remove unneeded features/columns in dataframe
@@ -172,7 +200,7 @@ def get_technical_data(stock_ticker, date_range):
 
 
 def main():
-    print(get_technical_data('TEL', get_dates_five_years()))
+    print(get_technical_data('AP', get_dates_five_years(testing=True)))
 
 
 if __name__ == '__main__':
